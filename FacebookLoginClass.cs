2b699dfc271f1d8f90d6b3a54a6b3d83bc96fa39
pavan205 <pavanreddi19@gmail.com>
2016-06-21 21:28:04 -0500

    Add files via upload

---
+++ FacebookLoginClass.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+
+namespace FacebookLoginTest
+{
+    class FacebookLoginClass
+    {
+
+        public static void Main()
+        {
+            // create the webdriver
+
+            IWebDriver d = new ChromeDriver(@"E:\Softwares\Testing\chromedriver_win32\chromedriver.exe");
+
+            // open url
+            d.Navigate().GoToUrl("http://www.facebook.com");
+            Console.WriteLine(" Facebook url is open sucessfully...");
+
+
+            // enter facebook email id in testbox
+            Thread.Sleep(5000);
+            d.FindElement(By.Id("email")).SendKeys("mnaidu.thota@gmail.com");
+            Console.WriteLine(" email id sending sucessfully...");
+
+            // enter password to text box
+            Thread.Sleep(5000);
+            d.FindElement(By.Id("pass")).SendKeys("lakshmi@143");
+            Console.WriteLine(" Facebook password sending sucessfully...");
+
+            // click on login 
+            Thread.Sleep(5000);
+            d.FindElement(By.Id("u_0_m")).Click();
+
+            Console.WriteLine(" Facebook login sucessfully...");
+
+            
+        }
+    }
+}
---
+++ FacebookUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace FacebookLoginUnitTestProject
+{
+    [TestClass]
+    public class FacebookUnitTest
+    {
+        [TestMethod]
+        public void Login()
+        {
+            // create the webdriver
+
+            IWebDriver d = new ChromeDriver(@"E:\Softwares\Testing\chromedriver_win32\chromedriver.exe");
+
+            // open url
+            d.Navigate().GoToUrl("http://www.facebook.com");
+           
+
+
+            // enter facebook email id in testbox
+            Thread.Sleep(5000);
+            d.FindElement(By.Id("email")).SendKeys("mnaidu.thota@gmail.com");
+        
+
+            // enter password to text box
+            Thread.Sleep(5000);
+            d.FindElement(By.Id("pass")).SendKeys("lakshmi@143");
+          
+
+            // click on login 
+            Thread.Sleep(5000);
+            d.FindElement(By.Id("u_0_m")).Click();
+
+           
+        }
+    }
+}
